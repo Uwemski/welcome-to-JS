cb6de97365cd57279eb9fbca7873adb729047e3a
Uffort Uwem <iconuwemfrank@gmail.com>
2023-04-22 14:37:10 +0100

    Made some modifications. Updated the round score when the score is one using a decision structure (which was the IF statement and TERNARY operator). The classList object was also introduced to show how to add and remove classes from our html structure using the DOM. It confused me a little but I/'m excited stillgit commit -m 'Made some modifications. Updated the round score when the score is one using a decision structure (which was the IF statement and TERNARY operator). The classList object was also introduced to show how to add and remove classes from our html structure using the DOM. It confused me a little but I'm excited stillgit add app.js

--- app.js
+++ app.js
@@ -16,10 +16,14 @@ scores = [0,1];
 roundScore = 0;
 activePlayer = 0; 
 
+/*document.querySelector('.current-score').textContent = '0';
+document.querySelector('.score').textContent = '0';  
+document.getElementById('name--0').textContent = 'uwemAbasi';
+document.getElementById('name--1').textContent = 'Blessing';*/
 
-document.querySelector('.dice').style.display= 'none';
-document.getElementById('score--0').textContent= '0';
-document.getElementById('current--0').textContent= '0';
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score--0').textContent = '0';
+document.getElementById('current--0').textContent = '0';
 document.getElementById('score--1').textContent = '0';
 document.getElementById('current--1').textContent = '0';
 
@@ -28,41 +32,30 @@ document.getElementById('current--1').textContent = '0';
 document.querySelector('.btn--roll').addEventListener('click', function() {
     //add the math random 
     var dice = Math.floor(Math.random()*6 ) +1;
-    document.querySelector(.dice).textContent = dice;
+    diceDOM = document.querySelector('.dice');
     
-    //display the dice picture
-    //document.querySelector('.btn').style.display = 'block'; 
-   
-    //2. Display the dice
-    //we saved the value of document.querySelector('.dice) into a diceDOM variable to avoid repeating ourselves
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display ='block';
-    diceDOM.src = 'dice-' + dice + '.png'; 
+    //display the dice picture and let it display a random number when clicked
+    diceDOM.style.display = 'block'; 
+    diceDOM.src = 'dice-' + dice + '.png';
 
-    //3. Update the roundScore IF the rolled number was NOT a 1
-    if ( dice !== 1){
-        //add
+    //3. If dice is 1 what should hapen and what not should happen
+    if ( dice !== 1) {
         roundScore += dice;
-        document.querySelector('#current--' + activePlayer).textContent = roundScore;
-    } else{
-        activePlayer === 0 ? activePlayer =1 : activePlayer = 0;
-        roundScore = 0;    
-        //next player's turn
+        document.getElementById('current--' +activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+        roundScore =0;
 
-        //document.getElementById('current--0').textContent = '0';
-        //document.getElementById('current--1').textContent = '0';
+        document.getElementById('current--0').textContent = '0';
+        document.getElementById('current--1').textContent = '0';
+
+        document.querySelector('.player--0').classList.remove('player--active');
+        document.querySelector('.player--1').classList.add('player--active');
     }
-    
-
-})
-
-//document.querySelector('#current--' + activePlayer).textContent = dice; 
-//document.querySelector('#current--' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x= document.querySelector('#score--0').textContent;
-//console.log(x);   
+});
 
 
-
+  
 
 
 
