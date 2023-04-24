ca2de6e5a8329b80c45741d73c46fed5bd433894
Uffort Uwem <iconuwemfrank@gmail.com>
2023-04-24 07:20:48 +0100

    Instead of using the classList object to add and remove when its a new player's turn, we found an efficient way of doing that by using toggle property

--- app.js
+++ app.js
@@ -49,11 +49,23 @@ document.querySelector('.btn--roll').addEventListener('click', function() {
         document.getElementById('current--0').textContent = '0';
         document.getElementById('current--1').textContent = '0';
 
-        document.querySelector('.player--0').classList.remove('player--active');
-        document.querySelector('.player--1').classList.add('player--active');
+        document.querySelector('.player--0').classList.toggle('player--active');
+        document.querySelector('.player--1').classList.toggle('player--active');
     }
 });
 
+document.querySelector('.btn--hold').addEventListener('click', function() { 
+    //Add CURRENT score to GLOBAL score
+
+
+    //Update the UI
+
+    
+    //Check if player won the game
+
+    
+
+})
 
   
 
