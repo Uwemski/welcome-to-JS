1941688d4697caeca6a11242413f2f093f8fe7c9
Uffort Uwem <iconuwemfrank@gmail.com>
2023-05-05 12:21:02 +0100

    made some modifications to the eventListener when the hold butto is clicked. We implemented a decison structure for the game app to know when the player get to or above 100 on global score. When that happens, there should be a change in the textContent of the current player(whch is the winner), and also a different style should be added to the winner.

--- app.js
+++ app.js
@@ -58,10 +58,11 @@ document.querySelector('.btn--hold').addEventListener('click', function() {
 
     
     //Check if player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name--' + activePlayer).textContent = "WINNER";
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name--' + activePlayer).textContent = "WINNER!!";
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player--' + activePlayer).classList.remove('player--active');
+        document.querySelector('.player--' + activePlayer).classList.add('player--winner');
     }else {
         //Next player's turn
         nextPlayer();
