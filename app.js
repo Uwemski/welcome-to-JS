e192c70cf4345c2565af409bd3cbe8cbffdc5e36
Uffort Uwem <iconuwemfrank@gmail.com>
2023-05-10 07:58:34 +0100

    finishing touches added. We learnt about the state variable andutilised its efficiency in our application. It feels really great to have coded my first javascript application. Praise God!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
  - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init(); 
 
@@ -22,45 +22,52 @@ document.getElementById('name--1').textContent = 'Blessing';*/
 
 
 document.querySelector('.btn--roll').addEventListener('click', function() { 
-    //create a variable to store result of random number
-    var dice = Math.floor(Math.random() *6) +1;
-    var diceDOM = document.querySelector('.dice') //for clean and efficient code 
+    
+    if (gamePlaying) {
+        //create a variable to store result of random number
+        var dice = Math.floor(Math.random() *6) +1;
+        var diceDOM = document.querySelector('.dice') //for clean and efficient code 
 
-    //display the dice when roll dice is clicked on the UI
-    diceDOM.style.display = 'block';
+        //display the dice when roll dice is clicked on the UI
+        diceDOM.style.display = 'block';
+        //use the dom to mainpulate image
+        diceDOM.src = "dice-" + dice + ".png";
 
-    //if the number is 1, next player's turn
-    if (dice != 1) {
-        roundScore += dice;
+        //if the number is 1, next player's turn
+        if (dice != 1) {
+            roundScore += dice;
 
-        document.getElementById('current--' + activePlayer).textContent = roundScore;
-    } else {
+            document.getElementById('current--' + activePlayer).textContent = roundScore;
+        } else {
         nextPlayer();
+        } 
     }
-
-    //use the dom to mainpulate image
-    diceDOM.src = "dice-" + dice + ".png";
 });
 
 
 document.querySelector('.btn--hold').addEventListener('click', function() { 
-    //Add CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
+    
+    if (gamePlaying) {
+        //Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
 
-    //Update the UI
-    document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
+        //Update the UI
+        document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
 
     
-    //Check if player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name--' + activePlayer).textContent = 'WINNER!!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player--' + activePlayer).classList.remove('player--active');
-        document.querySelector('.player--' + activePlayer).classList.add('player--winner');
-    }else {
+        //Check if player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name--' + activePlayer).textContent = 'WINNER!!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player--' + activePlayer).classList.remove('player--active');
+            document.querySelector('.player--' + activePlayer).classList.add('player--winner');
+            gamePlaying = false;
+        }else {
         //Next player's turn
         nextPlayer();
-    }
+        }
+    } 
+    
 });
 
 document.querySelector('.btn--new').addEventListener('click', init );
@@ -69,6 +76,8 @@ function init() {
     scores = [0, 0];
     roundScore = 0; 
     activePlayer = 0;
+    gamePlaying = true;
+
 
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score--0').textContent = '0';
